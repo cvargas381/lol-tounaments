0d4c4b0b788641493f83f1b60995dff4a1734125
cvargas381 <chris401110@gmail.com>
2013-01-31 12:00:12 -0600

    adds some layout files

---
+++ layout/nav.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+	<div class="navbar-inner">
+		<a class="brand" href="./">LoL Tournament Logistics</a>
+	  	<ul class="nav">
+			<li><a href="./?p=list_bands">Tournaments</a></li>
+			<li><a href="./?p=form_add_band">Add Logistics</a></li>
+		</ul>
+	</div>
+</div>
