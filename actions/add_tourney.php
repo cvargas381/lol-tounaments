505e6f4f7c9d98ea4ffac0c099049bd080f1e828
cvargas381 <chris401110@gmail.com>
2013-02-04 13:43:42 -0600

    new date widget is now fully function with form

--- actions/add_tourney.php
+++ actions/add_tourney.php
@@ -1,14 +1,14 @@
 <?php session_start() ?>
 <?php
 // validate that each piece of info was provided
-if($_POST['tourney_name'] != '' && $_POST['tourney_location'] != '' && $_POST['tourney_dates'] != '') {
+if($_POST['tourney_name'] != '' && $_POST['tourney_location'] != '' && $_POST['from'] != '' && $_POST['to'] != '') {
 
 	// Add this band to the CSV File
 	// (1) open the file for writing
 	$f = fopen('../data/tourneys.csv','a');
 
 	// (2) write the new band's info to the file
-	fwrite($f,"\n{$_POST['tourney_name']},{$_POST['tourney_location']},{$_POST['tourney_dates']}");
+	fwrite($f,"\n{$_POST['tourney_name']},{$_POST['tourney_location']},{$_POST['from']},{$_POST['to']}");
 
 	// (3) close the file
 	fclose($f);
--- views/list_tourneys.php
+++ views/list_tourneys.php
@@ -4,7 +4,8 @@
 			<tr>
 				<th>Name</th>
 				<th>Location</th>
-				<th>Dates</th>
+				<th>Start Date</th>
+				<th>End Date</th>
 				<th></th>
 			</tr>
 		</thead>
@@ -21,11 +22,13 @@ foreach($lines as $line) {
 	$parts = explode(',',$line);
 	$name = $parts[0];
 	$location = $parts[1];
-	$dates = $parts[2];
+	$start_date = $parts[2];
+	$end_date = $parts[3];
 	echo '<tr>';
 	echo 	"<td>$name</td>";
 	echo 	"<td>$location</td>";
-	echo 	"<td>$dates</td>";
+	echo 	"<td>$start_date</td>";
+	echo 	"<td>$end_date</td>";
 	// code for deleting confirmation
 	$onclick = "return confirm('Are you sure you want to delete $name?')";
 	echo 	"<td><a href=\"./?p=form_edit_tourney&tourney=$i\" class=\"btn btn-warning\"><i class=\"icon-edit icon-white\"></i></a> <a onclick=\"$onclick\" href=\"./actions/delete_tourney.php?linenum=$i\" class=\"btn btn-danger\"><i class=\"icon-trash icon-white\"></i></a></td>";
