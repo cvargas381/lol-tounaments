f7e2030ba680b730c5ca93db5f9e11bc9cc0c2e5
cvargas381 <chris401110@gmail.com>
2013-02-04 12:12:54 -0600

    changes header text

--- layout/nav.php
+++ layout/nav.php
@@ -1,6 +1,6 @@
 <div class="navbar">
 	<div class="navbar-inner">
-		<a class="brand" href="./">LoL Tournament Logistics</a>
+		<a class="brand" href="./">League of Legends Tournaments</a>
 	  	<ul class="nav">
 			<li><a href="./?p=list_tourneys">Tournaments</a></li>
 			<li><a href="./?p=form_add_tourney">Add Tournament</a></li>
